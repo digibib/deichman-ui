2481341dd85bf5bea875c481eaa52d34117eb82f
Bård N. Hovde <bnhovde@gmail.com>
2019-01-15 12:56:10 +0100

    Updated main imports file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-// Import all
+import './index.css';
+
+// Layout
+import Block from './components/Block';
+import Grid, { GridItem } from './components/Grid';
+
+// Components
+import Button from './components/Button';
+import Icon from './components/Icon';
+import Input from './components/Input';
+import Tag from './components/Tag';
+import TagList from './components/TagList';
 import Work from './components/Work';
 import WorkList from './components/WorkList';
-import Header from './components/Header';
-import Tag from './components/Tag';
-import Block from './components/Block';
-import Icon from './components/Icon';
-// import { H1, H2, H3, H4 } from './primitives/H';
-// import Block from './primitives/Block';
-// import Highlight from './primitives/Highlight';
 
-export { Work, WorkList, Header, Tag, Block, Icon };
-// export { Work, WorkList, Header, H1, H2, H3, H4, Button, Tag, Block, Highlight };
+export { Block, Grid, GridItem, Button, Icon, Input, Tag, TagList, Work, WorkList };
