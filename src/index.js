e531c551bcbdfd370c9b1d22d5582eac2dddb7f3
Bård N. Hovde <bnhovde@gmail.com>
2019-01-18 11:55:10 +0100

    Exposed List component

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import Accordion from './components/Accordion';
 import Button from './components/Button';
 import Icon from './components/Icon';
 import Input from './components/Input';
+import List from './components/List';
 import Select from './components/Select';
 import Table from './components/Table';
 import Tag from './components/Tag';
@@ -25,6 +26,7 @@ export {
   Button,
   Icon,
   Input,
+  List,
   Select,
   Table,
   Tag,
