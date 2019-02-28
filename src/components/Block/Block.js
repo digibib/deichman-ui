5aa159e8b9ceef2d0388c45e4c1588b90c3944d7
Bård N. Hovde <bnhovde@gmail.com>
2019-02-28 10:55:21 +0100

    FEATURE - First draft of responsive block

--- src/components/Block/Block.js
+++ src/components/Block/Block.js
@@ -4,24 +4,34 @@ import classNames from 'classnames';
 
 import './styles.css';
 
-const Block = ({ top, bottom, left, right, children }) => {
+const Block = ({ top, bottom, left, right, responsive, children }) => {
   const blockClass = classNames({
     block: true,
     [`block--top-${top}`]: top,
     [`block--bottom-${bottom}`]: bottom,
     [`block--left-${left}`]: left,
     [`block--right-${right}`]: right,
+    'block--responsive': responsive,
   });
 
   return <div className={blockClass}>{children}</div>;
 };
 
+Block.defaultProps = {
+  top: false,
+  bottom: false,
+  left: false,
+  right: false,
+  responsive: false,
+};
+
 Block.propTypes = {
   children: PropTypes.node.isRequired,
-  top: PropTypes.number,
-  bottom: PropTypes.number,
-  left: PropTypes.number,
-  right: PropTypes.number,
+  top: PropTypes.oneOfType([PropTypes.number, PropTypes.bool]),
+  bottom: PropTypes.oneOfType([PropTypes.number, PropTypes.bool]),
+  left: PropTypes.oneOfType([PropTypes.number, PropTypes.bool]),
+  right: PropTypes.oneOfType([PropTypes.number, PropTypes.bool]),
+  responsive: PropTypes.bool,
 };
 
 export default Block;
--- src/components/Block/Block.md
+++ src/components/Block/Block.md
@@ -21,3 +21,17 @@ import { Block } from 'deichman-ui';
   margin-top: var(--spacing-1);
 }
 ```
+
+#### Responsive version
+
+[WIP] - The responsive block will change it's top/bottom values between small/large screens based on a set of standard values. This only affects values > 5 as the smaller ones should remain the same. Use this for sections that require more vertical spacing on larger screens.
+
+### Props/modifiers:
+
+| prop name  | type   | without react        |
+| ---------- | ------ | -------------------- |
+| top        | number | block--top-{1-10}    |
+| right      | number | block--right-{1-10}  |
+| left       | number | block--left-{1-10}   |
+| bottom     | number | block--bottom-{1-10} |
+| responsive | bool   | block--responsive    |
