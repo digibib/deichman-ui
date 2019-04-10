fa1df6d7693a0913159ee7e6bd300c13603ac6ec
Bård N. Hovde <bnhovde@gmail.com>
2019-04-10 10:36:08 +0200

    FEATURE - added optional table row styles

--- src/components/Table/Row.js
+++ src/components/Table/Row.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 import './styles.css';
 
 const Row = ({ children }) => {
-  return <tr className="table__row">{children}</tr>;
+  const rowClass = classNames({
+    table__row: true,
+    'table__row--gray': gray,
+    'table__row--white': white,
+  });
+  return <tr className={rowClass}>{children}</tr>;
 };
 
 Row.propTypes = {
